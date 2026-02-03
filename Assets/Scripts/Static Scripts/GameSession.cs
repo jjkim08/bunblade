5143a93c6e84dc6bacf072b676d654c5bda9303f
JKgmr <70977623+JKgmr101@users.noreply.github.com>
2026-02-03 12:31:13 -0700

    Restore original GameSession class and remove testing code

--- Assets/Scripts/Static Scripts/GameSession.cs
+++ Assets/Scripts/Static Scripts/GameSession.cs
@@ -1,81 +1,5 @@
 using UnityEngine;
 
-/*
-// TESTING VERSION - Commented out for now
-public class GameSessionTesting : MonoBehaviour
-{
-    public static GameSession gs { get; private set; }
-
-    [HideInInspector] public PlayerState playerMember;
-    [HideInInspector] public EnemyState enemyMember;
-
-    [SerializeField] private EnemyStats demoEnemyForTesting;
-    [SerializeField] private PlayerStats demoPlayerStats;
-
-    private SaveSystem saveSystem;
-
-    void Awake()
-    {
-        if (gs != null)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        gs = this;
-        DontDestroyOnLoad(gameObject);
-
-        saveSystem = GetComponent<SaveSystem>();
-        if (saveSystem == null)
-            saveSystem = gameObject.AddComponent<SaveSystem>();
-
-        // TODO: Remove this line when done testing
-        InitializePlayer();
-
-        // TODO: Remove this line when done testing
-        if (demoEnemyForTesting != null)
-            InitializeBattle(demoEnemyForTesting);
-    }
-
-    private void InitializePlayer()
-    {
-        if (playerMember == null)
-        {
-            playerMember = new PlayerState(demoPlayerStats);
-
-            PlayerSaveData saved = saveSystem.LoadPlayer();
-            if (saved != null)
-            {
-                playerMember.currentHealth = saved.currentHealth;
-            }
-            else
-            {
-                // there is no save, todo: laterrrrr
-            }
-
-            playerMember.currentMana = 3;
-            playerMember.initializePushTurnIcons();
-        }
-    }
-
-    public void InitializeBattle(EnemyStats enemyToFight)
-    {
-        if (enemyToFight == null)
-            return;
-
-        enemyMember = new EnemyState(enemyToFight);
-        enemyMember.currentHealth = enemyToFight.baseMaxHealth;
-        enemyMember.initializePushTurnIcons();
-    }
-
-    public void SaveGame()
-    {
-        saveSystem.SavePlayer(playerMember);
-    }
-}
-*/
-
-// ORIGINAL VERSION - Restored
 public class GameSession : MonoBehaviour
 {
     public static GameSession gs { get; private set; }
@@ -134,4 +58,5 @@ public class GameSession : MonoBehaviour
         currentGold += amount;
         Debug.Log($"Gold added: {amount}. Total gold: {currentGold}");
     }
+
 }
